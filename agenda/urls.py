9f43924f3b0f325f2161472e549142f4d3f29ab2
Ulysses Lopes <ulysses.rlopes@gmail.com>
2022-04-04 17:50:36 -0300

    acrescentado formulário de inserção de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from  core import views
+from core import views
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -25,5 +25,7 @@ urlpatterns = [
     path('login/', views.login_user),
     path('login/submit', views.submit_login ),
     path('logout/', views.logout_user),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
 
 ]
